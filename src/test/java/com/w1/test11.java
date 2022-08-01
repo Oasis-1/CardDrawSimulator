1a5a0b4caf996e1ac9c5689623e256b6b4240fa3
明一帆 <114634@hichain.com>
2022-08-01 08:40:39 +0800

    随机一亿

--- src/test/java/com/w1/test11.java
+++ src/test/java/com/w1/test11.java
@@ -8,22 +8,25 @@ import org.junit.Test;
  * @date 2022-07-29 17:16
  */
 public class test11 {
-    private Long sum=100000L;
+    private Long sum=100000000L;
 
     @Test
     public void test13(){
+        //原神
         Genshin genshin = new Genshin(1,73L,6,0.6,101);
         genshin.tenFire(sum);
     }
 
     @Test
     public void Arknights13(){
+        //明日方舟
         Genshin genshin = new Genshin(1,50L,2,2,101);
         genshin.tenFire(sum);
     }
 
     @Test
     public void HearthStone13(){
+        //炉石
         Genshin genshin = new Genshin(1,sum+1,2,1,101);
         genshin.tenFire(sum);
     }
